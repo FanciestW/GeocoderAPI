58a3699140e1089f30fb67d3b2a0f14fbe28e5cc
William Lin <wlin26@yahoo.com>
2019-05-12 19:58:00 -0400

    Updated test and production code

--- src/index.ts
+++ src/index.ts
@@ -22,37 +22,79 @@ const mapClient = googleMaps.createClient({
 const redisClient = redis.createClient({
     host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 6379,
-    db: process.env.REDIS_DB || 'countries',
-    password: process.env.REDIS_PW || 'password',
+    // db: process.env.REDIS_DB || 'countries',
+    // password: process.env.REDIS_PW || 'password',
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send('OK');
-    console.log(process.env.GOOGLE_MAPS_API_KEY);
+app.get('/test_api_key', (req, res) => {
+    res.status(200).send(process.env.GOOGLE_MAPS_API_KEY);
+});
+
+app.get('/test', (req, res) => {
+    const location = req.query.location;
+    if (!location) {
+        res.status(400).send('Bad Request');
+        return;
+    }
+    redisClient.get(location, (redisErr, redisRes) => {
+        if (redisErr || redisRes === null) {
+            console.log('Does not exist, using API');
+            // Geocode an address.
+            mapClient.geocode({
+                address: location,
+            }, (mapErr, mapRes) => {
+                if (!mapErr) {
+                    if (mapRes.json.results === undefined || mapRes.json.results.length === 0) {
+                        redisClient.set(location, 'No Results');
+                        res.status(200).send('No Results');
+                    }
+                    const addrComponents = mapRes.json.results[0].address_components;
+                    const country = addrComponents[addrComponents.length - 1];
+                    const longName = country.long_name;
+                    console.log(`Settings ${location} in redis`);
+                    redisClient.set(location, longName);
+                    res.status(200).send(`Settings ${location} in redis with ${longName}`);
+                } else {
+                    res.status(500).send(mapErr);
+                }
+            });
+        } else {
+            console.log(location + ' Exists already');
+            res.status(200).send(`${location} exists already with value ${redisRes}`);
+        }
+    });
 });
 
 app.get('/country', (req, res) => {
     const location = req.query.location;
-    if (location) {
-        // Geocode an address.
-        mapClient.geocode({
-            address: location,
-        }, (err, response) => {
-            if (!err) {
-                if (response.json.results === undefined || response.json.results.length === 0) {
-                    res.status(200).send('No Results');
-                }
-                const addrComponents = response.json.results[0].address_components;
-                const country = addrComponents[addrComponents.length - 1];
-                const longName = country.long_name;
-                res.status(200).send(longName);
-            } else {
-                res.status(500).send(err);
-            }
-        });
-    } else {
+    if (!location) {
         res.status(400).send('Bad Request');
+        return;
     }
+    redisClient.get(location, (redisErr, redisRes) => {
+        if (redisErr || redisRes === null) {
+            // Geocode an address.
+            mapClient.geocode({
+                address: location,
+            }, (mapErr, mapRes) => {
+                if (!mapErr) {
+                    if (mapRes.json.results === undefined || mapRes.json.results.length === 0) {
+                        redisClient.set(location, 'No Results');
+                        res.status(200).send('No Results');
+                    }
+                    const addrComponents = mapRes.json.results[0].address_components;
+                    const country = addrComponents[addrComponents.length - 1];
+                    const longName = country.long_name;
+                    redisClient.set(location, longName);
+                    res.status(200).send(longName);
+                } else {
+                    res.status(500).send(mapErr);
+                }
+            });
+        } else {
+            res.status(200).send(redisRes);
+        }
+    });
 });
 
 app.get('/geocode', (req, res) => {
